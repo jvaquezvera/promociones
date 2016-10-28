4b44f8551b80f58b9370db963e859e69f369a4a3
workbench <>
2016-10-28 01:09:59 -0500

    {/Promociones/src/main/java/com/posadas/nochespremio/processor/PointsCalculator.java}

--- Promociones/src/main/java/com/posadas/nochespremio/processor/PointsCalculator.java
+++ Promociones/src/main/java/com/posadas/nochespremio/processor/PointsCalculator.java
@@ -93,7 +93,7 @@ public class PointsCalculator {
  		
  		for(RoomRateDTO roomRate : avail.getRoomRates()){
 // 			Calculo de puntos por noche
-System.out.println("roomRate: " + roomRate);
+System.out.println("roomRate: " + roomRate.getStartDate());
  			PaymentDTO payment = this.calculate(roomRate.getCurrency(), roomRate.getAmount(), factor, avail.getRateExchange());
  			payment.setIdentifier(identifier++);
  			roomRate.setPayment(payment);
