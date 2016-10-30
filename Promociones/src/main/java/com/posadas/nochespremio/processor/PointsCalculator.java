e183e38424144efdded3810f448380530f35bdba
workbench <>
2016-10-30 13:00:07 -0600

    {/Promociones/src/main/java/com/posadas/nochespremio/processor/PointsCalculator.java}

--- Promociones/src/main/java/com/posadas/nochespremio/processor/PointsCalculator.java
+++ Promociones/src/main/java/com/posadas/nochespremio/processor/PointsCalculator.java
@@ -123,13 +123,14 @@ public class PointsCalculator {
 				System.out.println("aplicando promocion.....apicando"+avail.getPorcentaje());
 				float descuento= 1- (avail.getPorcentaje()/100);
 				roomRate.setPromotion(true);
-				
+			roomRate.getPayment().setPercentage(avail.getPorcentaje());
 			roomRate.getPayment().setPointsPromo(Math.round(payment.getPoints() * descuento));
 			roomRate.getPayment().setCashPromo(payment.getCash() * descuento );
 			roomRate.getPayment().setMixedPointsPromo(Math.round(payment.getMixedPoints() * 					 descuento));							
 		
 			}else{
 				System.out.println("fuera de promocion.....apicando"+avail.getPorcentaje());
+			roomRate.getPayment().setPercentage(0);
 			roomRate.getPayment().setPointsPromo(payment.getPoints());
 			roomRate.getPayment().setCashPromo(payment.getCash());
 			roomRate.getPayment().setMixedPointsPromo(payment.getMixedPoints());							
